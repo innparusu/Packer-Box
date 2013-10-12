f23f77c1817f874fffd421e1e234dc2cdc49ca51
innparusu <innparusu95@yahoo.co.jp>
2013-10-12 17:06:48 +0900

    Add script

---
+++ packer/script/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+date > /etc/vagrant_box_build_time
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+
+rm -rf VBoxGuestAdditions_*.iso
