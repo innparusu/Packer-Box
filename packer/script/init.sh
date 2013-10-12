2513ffc193ffdddb458b4e043b9a157684a2a403
innparusu <innparusu95@yahoo.co.jp>
2013-10-13 00:14:54 +0900

    Complete Make box

--- packer/script/init.sh
+++ packer/script/init.sh
@@ -11,4 +11,5 @@ cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
 mkdir -p /vagrant
