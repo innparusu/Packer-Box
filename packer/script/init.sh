5a692f4666fc42569dbf2d5048ecbe1958515c99
innparusu <innparusu95@yahoo.co.jp>
2013-10-13 02:39:20 +0900

    Add comment init.sh

--- packer/script/init.sh
+++ packer/script/init.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
+#必要なパッケージを導入
 yum -y install gcc dkms make kernel-devel wget clang zsh
+
+#公開鍵の設定
 date > /etc/vagrant_box_build_time
 mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
 
+#GuestAdditionsのインストール
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+
+#共有フォルダを作成
 mkdir -p /vagrant
